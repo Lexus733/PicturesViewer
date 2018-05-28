1710dde56629425449cd6f640843360126432fb7
Lexus733 <super.walkman@yandex.ru>
2018-05-28 17:51:51 +0400

    Added menu and listeners

--- app/src/main/java/com/example/dmitry/picturesviewer/GeneralActivity.java
+++ app/src/main/java/com/example/dmitry/picturesviewer/GeneralActivity.java
@@ -1,7 +1,6 @@
 package com.example.dmitry.picturesviewer;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -10,7 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -18,7 +22,7 @@ import java.util.List;
 
 public class GeneralActivity extends AppCompatActivity {
 
-
+    public static final String LOG_MENU = "menu";
    public static final int REQUEST_IMAGE_CAPTURE = 0;
    public static final int IMAGE_REQUEST = 1;
 
@@ -27,25 +31,70 @@ public class GeneralActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private GridLayoutManager gridLayoutManager;
     private PicturesAdapter picturesAdapter;
+    private PicturesAdapter.OnItemClickListener listener;
+    private PicturesAdapter.OnItemLongClickListener listenerLong;
+    private List<Image> images;
 
 
-    List<Image> images = new ArrayList<>();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_general_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_sort_by_date:{
+                Log.d(LOG_MENU,"Sort by date pressed" + id);
+                Toast.makeText(getApplicationContext(),"By date",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.action_sort_by_size:{
+            Log.d(LOG_MENU,"Sort by size pressed" + id);
+                Toast.makeText(getApplicationContext(),"By size",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
 
+        images = new ArrayList<>();
+
         setInitialData();
 
-        picturesAdapter = new PicturesAdapter(this,images);
+        listener = new PicturesAdapter.OnItemClickListener() {
+            @Override
+            public void OnItemClick(Image item) {
+                Toast.makeText(getApplicationContext(),"Test Normal Click " + item.toString(),Toast.LENGTH_SHORT).show();
+            }
+        };
+        listenerLong = new PicturesAdapter.OnItemLongClickListener() {
+            @Override
+            public boolean OnItemLongClick(Image item) {
+                Toast.makeText(getApplicationContext(),"Image was deleted:  " + item.toString(),Toast.LENGTH_SHORT).show();
+                images.remove(item);
+                picturesAdapter.notifyDataSetChanged();
+                return true;
+            }
+        };
+
+        picturesAdapter = new PicturesAdapter(this,images,listener,listenerLong);
         gridLayoutManager = new GridLayoutManager(this,3);
 
         fab = (FloatingActionButton) findViewById(R.id.fabBtn_camera);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerListView);
-
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(picturesAdapter);
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,9 +119,26 @@ public class GeneralActivity extends AppCompatActivity {
 
     }
 
+    private void getAllFiles(){
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        Log.d("path","Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("size","Size: " + files.length);
+        for (int i = 0; i < files.length; i++){
+            Log.d("files","File:" + files[i].getName());
+        }
+    }
+
+//    public boolean deleteFile(String path){
+//        File file = new File(path);
+//        boolean deleted = file.delete();
+//        return deleted;
+//    }
 
     private void setInitialData(){
         images.add(new Image(R.drawable.android));
         images.add(new Image(R.drawable.ic_photo_camera_black_24dp));
+        images.add(new Image(R.drawable.bigandroid));
     }
 }
--- app/src/main/java/com/example/dmitry/picturesviewer/PicturesAdapter.java
+++ app/src/main/java/com/example/dmitry/picturesviewer/PicturesAdapter.java
@@ -1,26 +1,38 @@
 package com.example.dmitry.picturesviewer;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.util.List;
 
 public class PicturesAdapter extends RecyclerView.Adapter<PicturesAdapter.ViewHolder> {
 
+    public interface OnItemLongClickListener{
+        boolean OnItemLongClick(Image item);
+    }
+
+    public interface OnItemClickListener{
+        void OnItemClick(Image item);
+    }
+
+
     private LayoutInflater inflater;
     private List<Image> images;
+    private OnItemClickListener listener;
+    private OnItemLongClickListener listenerLong;
 
-    public PicturesAdapter(Context context, List<Image> images) {
+
+    public PicturesAdapter(Context context, List<Image> images, OnItemClickListener listener, OnItemLongClickListener listenerLong) {
         this.images = images;
         this.inflater = LayoutInflater.from(context);
+        this.listener = listener;
+        this.listenerLong = listenerLong;
+
     }
 
     @Override
@@ -32,16 +44,8 @@ public class PicturesAdapter extends RecyclerView.Adapter<PicturesAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull PicturesAdapter.ViewHolder holder, final int position) {
         final Image image = images.get(position);
-        holder.imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(inflater.getContext(),"Image tapped: " + images.get(position),Toast.LENGTH_SHORT).show();
-                Intent  i = new Intent(inflater.getContext(),PicturesView.class);
-                i.putExtra("image",image.getImage());
-                inflater.getContext().startActivity(i);
-            }
-        });
         holder.imageView.setImageResource(image.getImage());
+        holder.bind(image,listener,listenerLong);
     }
 
     @Override
@@ -56,6 +60,21 @@ public class PicturesAdapter extends RecyclerView.Adapter<PicturesAdapter.ViewHo
         ViewHolder(View v){
             super(v);
             imageView = (ImageView) v.findViewById(R.id.picture_item);
+        }
+
+        public void bind(final Image image,final OnItemClickListener listener, final OnItemLongClickListener listenerLong){
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.OnItemClick(image);
+                }
+            });
+            imageView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    return listenerLong.OnItemLongClick(image);
+                }
+            });
         }
     }
 }
